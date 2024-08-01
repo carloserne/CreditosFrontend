988fe621371a3e9198e3ab1552b5124c6c8ad2ef
David Figueroa Rodríguez <david.figueroar0777@gmail.com>
2024-07-31 20:00:15 -0600

    Se agrego funcionalidad a la simulacion

--- src/app/services/simulador.service.ts
+++ src/app/services/simulador.service.ts
@@ -3,14 +3,16 @@ import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { ISimulador } from '../interfaces/simulador';
+import { ToastrService } from 'ngx-toastr';
+import { IAmortizacion } from '../interfaces/amortizacion';
 
 @Injectable({
     providedIn: 'root'
 })
 export class SimuladorService {
-    private apiUrl = 'http://localhost:8080/api/simulador';
+    private apiUrl = 'https://localhost:5000/api/simulacion';
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private toastr: ToastrService) { }
 
     calcularSimulador(): Observable<ISimulador> {
         return this.http.get<ISimulador>(this.apiUrl).pipe(
@@ -27,5 +29,24 @@ export class SimuladorService {
                             `Error: ${error.error}`;
         }
         return throwError(() => new Error(errorMessage));
+    }
+
+    obtenerSimulacion(datosSimulacion: ISimulador): Observable<IAmortizacion[]> {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            return throwError(() => new Error('No token found'));
+        }
+
+        const headers = {
+            'Content-Type': 'application/json',
+        };
+
+        return this.http.post<IAmortizacion[]>(this.apiUrl, datosSimulacion, { headers }).pipe(
+            catchError(error => {
+                console.error('Error obtaining simulacion:', error);
+                this.toastr.error('No se pudo realizar la simulación. Inténtelo de nuevo más tarde.', 'Error');
+                return throwError(() => new Error('Error saving simulacion'));
+            })
+        );
     }
 }
